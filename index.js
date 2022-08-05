2febd9b8f051a3a8361a84e2e98d9a5eb6903094
Tparfaite <parfaitetwagira@gmail.com>
2022-08-05 09:05:58 +0200

    Feat:commit change in css -Add some properties [Finishes#]

---
+++ index.js
@@ -0,0 +1,53 @@
+const body=document.getElementsByTagName("body")[0];
+console.log(body.children);
+// const head=document.getElementsByClassName("togle");
+// console.log(head);
+const header=Array.from(document.getElementsByClassName("togle"));
+console.log(header);
+header.map(item=>{
+    item.firstElementChild.addEventListener("click",e=>{
+       e.target.nextElementSibling.classList.toggle("hide");
+    })
+})
+
+// const bye=Array.from(head);
+// console.log(bye);
+// bye.map(item=>{
+//     item.firstElementChild.addEventListener("click",e=>{
+//         e.target.nextElementSibling.classList.toggle("bbbb");
+
+//     })
+// })
+
+
+
+const video=document.getElementById("video");
+
+const btn=document.getElementById("btn");
+btn.addEventListener("click",e=>{
+    e.target.innerHTML="song";
+    btn.style.backgroundColor="yellow";
+   btn.style.color="red";
+ video.classList.toggle("videoHide");
+   
+})
+const upper=document.createElement("div");
+body.insertBefore(upper,body.children[0]);
+upper.classList.add("upper");
+
+const righter=document.createElement("div");
+body.insertBefore(righter,upper);
+righter.classList.add("righter");
+
+const click=document.createElement("button");
+click.classList.add("click");
+click.innerHTML="click";
+body.insertBefore(click,body.children[0]);
+
+click.addEventListener("click",e=>{
+righter.classList.toggle("right");
+upper.classList.toggle("up");
+})
+
+
+
